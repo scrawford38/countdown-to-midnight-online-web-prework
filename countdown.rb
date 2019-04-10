9f1899726d7e531db409eb604630b69111f3171e
Sally Crawford <sally.crawford@gmail.com>
2019-04-10 12:20:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
-#write your code here
 
-def countdown
+def countdown(number)
+  counter=10
+  while counter>0
+  puts "#{number} SECOND(S)!"
+  counter-=1
+end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
+  "#{number} SECOND(S)!"
+  "HAPPY NEW YEAR!"
 end
